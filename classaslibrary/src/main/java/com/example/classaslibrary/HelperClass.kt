a0712720d07e476075c89512552412f56cb2720d
KashbullinRail <kashbullinrail@gmail.com>
2022-12-09 16:45:02 +0300

    Initial commit create lib classaslibrary and HelperClass

---
+++ classaslibrary/src/main/java/com/example/classaslibrary/HelperClass.kt
@@ -0,0 +1,12 @@
+package com.example.classaslibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class HelperClass {
+
+    fun connectLibrary(context: Context){
+        Toast.makeText(context, "Library connect Success", Toast.LENGTH_SHORT).show()
+    }
+
+}
