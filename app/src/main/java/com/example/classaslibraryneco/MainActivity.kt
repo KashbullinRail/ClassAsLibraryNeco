996f9a2f16810e55c3b91d09b49332dbbf7bac00
KashbullinRail <kashbullinrail@gmail.com>
2022-12-09 16:58:13 +0300

    attach R.layout.connect in MainActivity.kt

--- app/src/main/java/com/example/classaslibraryneco/MainActivity.kt
+++ app/src/main/java/com/example/classaslibraryneco/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.classaslibraryneco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.classaslibrary.HelperClass
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.classaslibrary.R.layout.connect)
+
+        val btn = findViewById<Button>(com.example.classaslibrary.R.id.btnConnect)
 
         val helpers = HelperClass()
 
